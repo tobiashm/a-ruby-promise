eb133bbb636809aeeb2ea044b6f2c15f1c032c87
Tobias Haagen Michaelsen <tobias.michaelsen@gmail.com>
2013-11-17 02:30:58 +0100

    Avoid hiding assertion failures in `teardown`.
    We don’t need to use a generalized `eventually` which listens for assertion errors in teardown, as we know what we are waiting for.

--- spec/promises_aplus.rb
+++ spec/promises_aplus.rb
@@ -4,24 +4,16 @@ module PromisesAplus
   end
 
   def teardown
-    eventually { @done.must_equal true }
+    time_limit = Time.now + 2
+    loop do
+      return if @done == true
+      raise Minitest::Assertion.new("`done` was never called") if Time.now >= time_limit
+      sleep 0.05
+    end
   end
 
   def done!
     @done = true
-  end
-
-  def eventually(&block)
-    time_limit = Time.now + 2
-    loop do
-      begin
-        yield
-      rescue Minitest::Assertion => error
-      end
-      return if error.nil?
-      raise error if Time.now >= time_limit
-      sleep 0.05
-    end
   end
 
   def self.test_fulfilled(base, value, &test)
